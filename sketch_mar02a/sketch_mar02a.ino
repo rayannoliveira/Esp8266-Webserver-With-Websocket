cff9bbe404fa8eba1198636fc14e6ebf2a405df3
Rayanne Oliveira <rayanneo390@gmail.com>
2020-05-21 16:18:01 -0300

    Esp8266
    Esp8266 com websocket por meu de scipt

---
+++ sketch_mar02a/sketch_mar02a.ino
@@ -0,0 +1,24 @@
+void setup() 
+{
+  // Colocamos o pino 12 do Arduino como OUTPUT (saída)
+  pinMode(12, OUTPUT);
+}
+
+// Este código é chamado automáticamente pelo Arduino, ficará em
+// loop até que seu Arduino seja desligado
+void loop() 
+{
+  // Ativamos o pino 12 (colocando 5v nele)  
+  digitalWrite(13, HIGH);
+
+  // Aguardamos 1 segundo
+  delay(2000);
+
+  // Desligamos o pino 12
+  digitalWrite(13, LOW);
+
+  // Aguardamos mais um segundo
+  delay(1000);
+
+  // Este código irá se repetir eternamente
+}
